855d7e89d24c544edfe3b8b00776530ca99525fc
Jeferson Antonio Mesquita <je_2742@hotmail.com>
2021-04-22 21:03:42 -0300

    Aula 03
    Página Home,
    Roteamento no NextJs,
    Página Episódio,
    Páginas estáticas e dinâmicas

--- aulas/podcastrnext/src/pages/index.tsx
+++ aulas/podcastrnext/src/pages/index.tsx
@@ -1,16 +1,18 @@
-import { GetStaticProps } from 'next';
+import { GetStaticProps } from "next";
+import Image from 'next/image';
+import Link from 'next/link';
 import { format, parseISO } from 'date-fns';
-import  ptBR  from 'date-fns/locale/pt-BR';
-import { api } from '../services/api';
-import { convertDurationToTimeString } from '../utils/convertDurationToTimeString';
+import ptBR from 'date-fns/locale/pt-BR';
+
+import { api } from "../services/api";
+import { convertDurationToTimeString } from "../utils/convertDurationToTimeString";
 
 import styles from './home.module.scss';
 
 type Episode = {
   id: string;
   title: string;
-  thumbnail: string;
-  description: string;
+  thumbnail: string;  
   members: string;
   duration: number;
   durationAsString: string;
@@ -23,41 +25,98 @@ type HomeProps = {
   allEpisodes: Episode[];
 }
 
-export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {  
+export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
   return (
     <div className={styles.homepage}>
       <section className={styles.latestEpisodes}>
-        <h2>Últimos Lançamentos</h2>
+        <h2>Últimos lançamentos</h2>
 
         <ul>
           {latestEpisodes.map(episode => {
             return (
               <li key={episode.id}>
-                <a href="">{episode.title}</a>
+                <Image                   
+                  width={192}
+                  height={192}
+                  src={episode.thumbnail} 
+                  alt={episode.title}
+                  objectFit="cover"
+                />
+
+                <div className={styles.episodeDetails}>
+                  <Link href={`/episodes/${episode.id}`}>
+                    <a>{episode.title}</a>
+                  </Link>
+                  <p>{episode.members}</p>
+                  <span>{episode.publishedAt}</span>
+                  <span>{episode.durationAsString}</span>
+                </div>
+
+                <button type="button">
+                  <img src="/play-green.svg" alt="Tocar episódio"/>
+                </button>
               </li>
-            )
+            );
           })}
         </ul>
       </section>
 
       <section className={styles.allEpisodes}>
+          <h2>Todos Episódios</h2>
 
+          <table cellSpacing={0}>
+            <thead>
+              <tr>
+                <th></th>
+                <th>Podcast</th>
+                <th>Integrantes</th>
+                <th>Data</th>
+                <th>Duração</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              {allEpisodes.map(episode => {
+                return(
+                  <tr key={episode.id}>
+                    <td style={{ width: 72}}>
+                      <Image
+                        width={120}
+                        height={120}
+                        src={episode.thumbnail}
+                        alt={episode.title}
+                        objectFit="cover"
+                      />
+                    </td>
+                    <td>
+                      <a href="">{episode.title}</a>
+                    </td>
+                    <td>{episode.members}</td>
+                    <td style={{ width: 80 }}>{episode.publishedAt}</td>
+                    <td>{episode.durationAsString}</td>
+                    <td>
+                      <button type="button">
+                        <img src="/play-green.svg" alt="Tocar episódio"/>
+                      </button>
+                    </td>
+                  </tr>
+                )
+              })}
+            </tbody>
+          </table>
       </section>
     </div>
   )
 }
 
-export const getStaticProps: GetStaticProps = async () => { // SSR e SSG - Exemplo
+export const getStaticProps: GetStaticProps = async () => {
   const { data } = await api.get('episodes', {
-    params: { 
+    params: {
       _limit: 12,
       _sort: 'published_at',
-      _order: 'desc'
+      _order: 'desc',
     }
-  })
-
-
-  
+  });
 
   const episodes = data.map(episode => {
     return {
@@ -68,13 +127,12 @@ export const getStaticProps: GetStaticProps = async () => { // SSR e SSG - Exemp
       publishedAt: format(parseISO(episode.published_at), 'd MMM yy', { locale: ptBR }),
       duration: Number(episode.file.duration),
       durationAsString: convertDurationToTimeString(Number(episode.file.duration)),
-      description: episode.description,
       url: episode.file.url,
-    };
-  })
+    }
+  });
   
   const latestEpisodes = episodes.slice(0, 2);
-  const allEpisodes = episodes.slice(2, episodes.length);
+  const allEpisodes = episodes.slice(2, episodes.length); 
 
   return {
     props: {
